2e8567a4aceae95bb4afb26a3a7e678a7ba29c6c
Julio Herrera <jcherrera@bgcapitalgroup.mx>
2022-08-30 16:58:14 -0500

    Sistema para crear xml

---
+++ Models/Modelfact.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlFromSqlServer.Models
+{
+    public class ModelFact
+
+
+    {
+        public string uuid { get; set; }
+        public string motivo { get; set; }
+        public string status { get; set; }
+
+        public string folio { get; set; }
+        public string fecha { get; set; }
+        public string serie { get; set; }
+        public string rfc { get; set; }
+        public string pdfAndXmlDownload { get; set; }
+        public string pdfDownload { get; set; }
+        public string xmlDownload { get; set; }
+        public string monto { get; set; }
+        public string tipoMoneda { get; set; }
+        public string consecutivo { get; set; }
+        public string billto { get; set; }
+        public string segmento { get; set; }
+        public string estatus { get; set; }
+        public string economico { get; set; }
+        public string flota { get; set; }
+
+        public string ord_hdrnumber { get; set; }
+        public string tcfix { get; set; }
+        private const string facturas = "select folio as Folio,fhemision as Fecha, nombrecliente as Cliente, idreceptor from VISTA_fe_Header";
+        private const string facturasClientes = "select distinct  idreceptor from vista_fe_header";
+        private const string facturasPorProcesar = "select * from VISTA_fe_Header where  idreceptor not in ('liverpol','GLOBALIV','LIVERTIJ','SFERALIV','FACTUMLV','LIVERDED')";
+        private const string facturasPorProcesarLivepool = "select * from VISTA_fe_Header where idreceptor in ('liverpol','GLOBALIV','LIVERTIJ','SFERALIV','FACTUMLV','LIVERDED')";
+        private const string facturaAdendaReferencia = "select ref_number, ref_type from referencenumber where ord_hdrnumber = @orden and (ref_type = 'ADEHOJ' or ref_type = 'ADEPED' or ref_type = 'LPROV')";
+        private const string datosFactura = "select * from VISTA_fe_Header where folio = @factura";
+        private const string detalle = "select * from vista_Fe_detail where folio = @factura";
+        private const string detalle33 = "select * from vista_Fe_detail where folio = @factura";
+        private const string invoice = "select ivh_invoicestatus,ivh_mbnumber,ivh_ref_number from invoiceheader where ivh_invoicenumber = @factura";
+        private const string updateTrans = "update invoiceheader set ivh_ref_number = @idComprobante where ivh_invoicenumber = @fact";
+        private const string updateTransMaster = "update invoiceheader set ivh_ref_number = @idComprobante where ivh_mbnumber  = @master";
+        private const string insertaGeneradas = "insert into VISTA_Fe_generadas (nmaster,invoice,serie,idreceptor,fhemision,total,moneda,rutapdf,rutaxml,imaging,bandera,\r\n            provfact,status,ultinvoice,hechapor,orden,rfc) values (@master,@factura,@serie,@idreceptor,@fhemision,@total,@moneda,\r\n            @rutapdf,@rutaxml,@imaging,@bandera,@provfactura,@status,@ultinvoice,@hechapor,@orden,@rfc)";
+        private const string parmFactura = "( select case when(select ivh_mbnumber from invoiceheader with (nolock) where ivh_invoicenumber = @factura) = 0 then @factura else (select max(ivh_invoicenumber) from invoiceheader with (nolock) where ivh_mbnumber = (select ivh_mbnumber from invoiceheader with (nolock) where ivh_mbnumber != 0 and ivh_invoicenumber = @factura)) end)";
+        private const string masterFactura = "select * from vista_fe_header where ultinvoice = @parmFact";
+        private const string minInvoice = "select invoice from vista_fe_header where ultinvoice = @parmFact";
+        private const string P_fact = "@factura";
+        private const string P_idComprobante = "@idComprobante";
+        private const string P_master = "@master";
+        private const string P_fact2 = "@fact";
+        private const string P_pfact = "@parmFact";
+        private const string P_invoice = "@lastInvoice";
+        private const string P_serie = "@serie";
+        private const string P_idReceptor = "@idreceptor";
+        private const string P_fhemision = "@fhemision";
+        private const string P_total = "@total";
+        private const string P_moneda = "@moneda";
+        private const string P_rutaPdf = "@rutapdf";
+        private const string P_rutaXML = "@rutaxml";
+        private const string P_imaging = "@imaging";
+        private const string P_bandera = "@bandera";
+        private const string P_provFact = "@provfactura";
+        private const string P_status = "@status";
+        private const string P_ultinvoice = "@ultinvoice";
+        private const string P_hechapor = "@hechapor";
+        private const string P_orden = "@orden";
+        private const string P_rfc = "@rfc";
+        private string _ConnectionString;
+
+        public ModelFact()
+        {
+            this._ConnectionString = new Connection().connectionString;
+        }
+        
+       
+        public DataTable xml()
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection connection = new SqlConnection(this._ConnectionString))
+            {
+                using (SqlCommand selectCommand = new SqlCommand("select * from(select trl_number as Economico, (select replace(replace(name,'&',' AND '),'/','') from labelfile where labeldefinition = 'fleet' and abbr=  trl_fleet) as flota from trailerprofile where trl_status <> 'OUT') as q", connection))
+                {
+                    selectCommand.CommandType = CommandType.Text;
+                    selectCommand.CommandTimeout = 100000;
+                    using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(selectCommand))
+                    {
+                        try
+                        {
+                            selectCommand.Connection.Open();
+                            sqlDataAdapter.Fill(dataTable);
+                        }
+                        catch (SqlException ex)
+                        {
+                            string message = ex.Message;
+                        }
+                    }
+                }
+            }
+            return dataTable;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using XmlFromSqlServer.Models;
+
+namespace XmlFromSqlServer
+{
+    public class Program
+    {
+        static storedProcedure sql = new storedProcedure("miConexion");
+        public static FacLabControler facLabControler = new FacLabControler();
+        static void Main(string[] args)
+        {
+            Program muobject = new Program();
+
+            muobject.Extraer();
+        }
+        public void Extraer()
+        {
+            //CODIGO PARA GENERAR XML DESDE UNA CONSULTA SQL SERVER
+
+            DataTable gxml = facLabControler.xml();
+            XDocument document = new XDocument(new XDeclaration("1.0", "utf-8", null));
+            XElement nodoRaiz = new XElement("Tractors");
+            document.Add(nodoRaiz);
+
+            foreach (DataRow iseg in gxml.Rows)
+            {
+
+                string economico = iseg["Economico"].ToString();
+                string flota = iseg["flota"].ToString();
+
+                XElement tractor = new XElement("Tractor");
+                tractor.Add(new XElement("Economico", economico));
+                tractor.Add(new XElement("flota", flota));
+                nodoRaiz.Add(tractor);
+
+
+            }
+
+
+            document.Save(@"C:\Archivos\Xml\exml.xml");
+        }
+    }
+}
